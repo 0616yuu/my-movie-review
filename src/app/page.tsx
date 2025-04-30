9bc64191997e9545aa2c9a31aa7191a1baf2f018
憂 <miyazakiyuu@MacBook-Pro.local>
2025-04-30 11:02:40 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function Page() {
+  const [selectedGenre , setSelectedGenre] = useState<string>('');
+  return (
+    <main className='p-4'>
+      
+    </main>
+  );
+}
---
+++ src/component/Header.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React, { useState } from 'react';
+
+type Props = {};
+
+const ComponentName: React.FC<Props> = (props) => {
+  const [state, setState] = useState<string>('');
+
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default ComponentName;
